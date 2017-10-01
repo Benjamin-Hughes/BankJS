46558f53fb89736e9100226bd49d8b48bafed141
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-10-01 15:29:13 +0100

    Impliment module pattern for Account object

--- src/Account.js
+++ src/Account.js
@@ -1,25 +1,29 @@
-function Account(name) {
-  this.name = name;
-  this.balance = 0;
-  this.transactions = [];
-}
+var Account = (function() {
+  function AnAccount(name) {
+    this.name = name;
+    this.balance = 0;
+    this.transactions = [];
+  }
 
-Account.prototype = {
-  owner: function() {
-    return this.name;
-  },
-  addFunds: function(amount) {
-    this.balance += amount;
-    var transaction = new Transaction(this.createDate);
-    console.log(transaction);
-    this.transactions.push(transaction);
-  },
-  withdrawFunds: function(amount) {
-    this.balance -= amount;
-  },
-  createDate: function() {
-    var rawDate = new Date();
-    var date = rawDate.toUTCString();
-  },
+  AnAccount.prototype = {
+    owner: function() {
+      return this.name;
+    },
+    addFunds: function(amount) {
+      this.balance += amount;
+      var transaction = new Transaction(this.createDate);
+      console.log(transaction);
+      this.transactions.push(transaction);
+    },
+    withdrawFunds: function(amount) {
+      this.balance -= amount;
+    },
+    createDate: function() {
+      var rawDate = new Date();
+      var date = rawDate.toUTCString();
+    },
 
-};
+  };
+
+  return AnAccount;
+}());
